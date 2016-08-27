8fa2fa34c8778a2a10530c8e1b5e4265985b8c42
lmh2072005 <lmh2072005@126.com>
2016-08-27 18:26:06 +0800

    update component

--- src/components/app1.vue
+++ src/components/app1.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="app">
         hello {{name}}
-        <span v-on:click="showMsg()">show message</span>
+        <p>
+            <span v-on:click="showMsg()">点击我向父组件派发事件</span>
+        </p>
     </div>
 </template>
 
@@ -11,16 +13,19 @@
               return {
                   name : 'world'
               }
+          },
+          methods : {
+              showMsg : function(){
+                  alert(this.name);
+                  this.$dispatch('child-msg', this.name); //向父组件派发事件，事件沿着父链冒泡，冒泡过程中第一次触发回调之后自动停止冒泡，除非回调明确返回 true
+              }
           }
-          /*methods : function(){
-              return {
-                  showMsg : function(){
-                        alert(this.name);
-                  }
-              };
-
-          }*/
     };
+    /*
+    * 使用 $on() 监听事件；
+     使用 $emit() 在它上面触发事件；
+     使用 $dispatch() 派发事件，事件沿着父链冒泡；
+     使用 $broadcast() 广播事件，事件向下传导给所有的后代。*/
 </script>
 <style>
     .app{
--- src/js/controllers/test.js
+++ src/js/controllers/test.js
@@ -1,10 +1,22 @@
-/**
- * Created by aaronmhliu on 2016/8/26.
- */
 
 var app = require('../../components/app1.vue');
 new Vue({
     el: 'body',
+    data : {
+        message : 'test'
+    },
+    methods : {
+        /*handleIt : function(msg){
+            alert(0)
+            console.log(msg);
+        }*/
+    },
+    events : {
+        'child-msg' : function(msg){
+            alert(1);
+            this.message = msg;
+        }
+    },
     components: {
         app: app
     }

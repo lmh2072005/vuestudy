3a8b623c84acf4b594fdf65fcda728bd1ea453d2
lmh2072005 <lmh2072005@126.com>
2016-12-11 23:02:43 +0800

    add 2.0 study

---
+++ vue2.0/src/app.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <h2>hello vue2.0</h2>
+        <router-link to="/index">home</router-link>
+        <router-link to="/video">video</router-link>
+        <router-view></router-view>
+    </div>
+</template>
+
